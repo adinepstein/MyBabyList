8d4c49d1099fc53013b7202b47d30a2de7b942f8
adinepstein <adin.epstein@intel.com>
2018-06-14 21:02:53 +0300

    adding date dialog + user firebase

--- app/src/main/java/com/example/adinepst/mybabylist/Model/Firebase/ModelFirebaseUsers.java
+++ app/src/main/java/com/example/adinepst/mybabylist/Model/Firebase/ModelFirebaseUsers.java
@@ -1,10 +1,49 @@
 package com.example.adinepst.mybabylist.Model.Firebase;
 
+import com.example.adinepst.mybabylist.UserDetailsFragment;
+import com.example.adinepst.mybabylist.Utils.PostData;
 import com.example.adinepst.mybabylist.Utils.UserData;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ModelFirebaseUsers {
+    final static String HEADNODE ="Users";
+    static private ValueEventListener eventListener;
 
      public void addUser(UserData ud) {
+         DatabaseReference dbRef= FirebaseDatabase.getInstance().getReference().child(HEADNODE);
+         dbRef.child(ud.getEmail()).setValue(ud);
+    }
+
+
+
+
+
+    public interface GetUserListener{
+        public void onSuccess(UserData ud);
+    }
+
+    public void getUser(String email, final GetUserListener listener){
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference().child(HEADNODE).child(email);
+        dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                UserData ud= dataSnapshot.getValue(UserData.class);
+                listener.onSuccess(ud);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
     }
+
 }
